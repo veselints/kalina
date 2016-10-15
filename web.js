49e4ea97d6cf717199aef6da2c84a61d54ad00e8
veselints <veselintsvetanov@gmail.com>
2016-10-15 13:09:32 +0300

    Implementing simple express server

--- web.js
+++ web.js
@@ -1,7 +1,6 @@
-var gzippo = require('gzippo');
-var express = require('express');
-var app = express();
+    var express  = require('express');
+    var app      = express();
 
-app.use(express.logger('dev'));
-app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-app.listen(process.env.PORT || 5000);
+    app.use(express.static(__dirname + '/dist'));
+    app.listen(process.env.PORT || 5000);
+    console.log("App listening on port 5000");
